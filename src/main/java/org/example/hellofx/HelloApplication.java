6d5cbc0c298ca76d9e3433ea16be5670dce0685e
lwrb172 <lwrbielak@gmail.com>
2024-09-11 23:50:59 +0200

    Stages

--- src/main/java/org/example/hellofx/HelloApplication.java
+++ src/main/java/org/example/hellofx/HelloApplication.java
@@ -2,17 +2,42 @@ package org.example.hellofx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image("polska.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Stage Demo Program");
+        stage.setWidth(500);
+        stage.setHeight(500);
+        stage.setResizable(false);
+//        stage.setX(50);
+//        stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("q"));
+
         stage.setScene(scene);
         stage.show();
     }
