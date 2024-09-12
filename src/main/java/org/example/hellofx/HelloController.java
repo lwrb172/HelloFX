06ed589bc1dfdb09b4d8912c70c102dbb8383133
lwrb172 <lwrbielak@gmail.com>
2024-09-12 15:31:33 +0200

    SceneBuilder - event handling

--- src/main/java/org/example/hellofx/HelloController.java
+++ src/main/java/org/example/hellofx/HelloController.java
@@ -1,14 +1,32 @@
 package org.example.hellofx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Circle myCircle;
+    private double x;
+    private double y;
+    
+    public void up(ActionEvent e) {
+        System.out.println("UP");
+        myCircle.setCenterY(y -= 10);
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void down(ActionEvent e) {
+        System.out.println("DOWN");
+        myCircle.setCenterY(y += 10);
+    }
+
+    public void right(ActionEvent e) {
+        System.out.println("RIGHT");
+        myCircle.setCenterX(x += 10);
+    }
+
+    public void left(ActionEvent e) {
+        System.out.println("LEFT");
+        myCircle.setCenterX(x -= 10);
     }
 }
